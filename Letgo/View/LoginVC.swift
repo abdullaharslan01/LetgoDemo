ee4e2884813cd86e50f37292779512010c421521
abdullaharslan01 <abdullaharslan1473@gmail.com>
2024-04-22 18:19:37 +0300

    Register Page Designed

--- Letgo/View/LoginVC.swift
+++ Letgo/View/LoginVC.swift
@@ -2,39 +2,41 @@ import UIKit
 
 class LoginVC: UIViewController {
 
-    @IBOutlet weak var email: UITextField!
+    // MARK: - UI Elements
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var emailLabel: UILabel!
-    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
+    
     @IBOutlet weak var nextButton: UIButton!
+    
+    var isEmailValid = false
+    var isPasswordValid = false
+    // MARK: - View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-        email.delegate = self
-        password.delegate = self
-        updateButtonAppearance(isValidEmail: false, isValidPassword: false)
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        updateButtonAppearance()
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
+    // MARK: - Actions
 
     @IBAction func nextButtonTapped(_ sender: Any) {
-        
-        //print(email.text, password.text)
+        print(emailTextField.text ?? "", passwordTextField.text ?? "")
     }
 
     @IBAction func registerButtonTapped(_ sender: Any) {
-        
         performSegue(withIdentifier: SegueNames.registerVC.rawValue, sender: nil)
-        
-        
     }
 
-    func updateButtonAppearance(isValidEmail: Bool, isValidPassword: Bool) {
-        if isValidEmail && isValidPassword {
+    // MARK: - Helper Methods
+
+    func updateButtonAppearance() {
+        
+        if isEmailValid && isPasswordValid {
             nextButton.isEnabled = true
             nextButton.backgroundColor = UIColor(named: "main")
             nextButton.setTitleColor(.white, for: .normal)
@@ -48,27 +50,43 @@ class LoginVC: UIViewController {
 }
 
 extension LoginVC: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+           if textField == emailTextField {
+               
+               let currentText = (textField.text ?? "") as NSString
+               let newText = currentText.replacingCharacters(in: range, with: string)
+                isEmailValid = isValidEmail(newText)
+               emailLabel.textColor = isEmailValid ? .black : UIColor(named: "main")
+               
+               emailTextField.textColor = isEmailValid ? .black : UIColor(named: "main")
+               
+           } else if textField == passwordTextField {
 
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        guard let emailText = email.text, let passwordText = password.text else { return }
+               let currentText = (textField.text ?? "") as NSString
+               let newText = currentText.replacingCharacters(in: range, with: string)
+                isPasswordValid = isValidPassword(newText)
+               
+               passwordTextField.textColor = isPasswordValid ? .black : UIColor(named: "main")
+               passwordLabel.textColor = isPasswordValid ? .black : UIColor(named: "main")
+               
+           }
+        updateButtonAppearance()
+           return true
+       }
 
-        let isEmailValid = isValidEmail(emailText)
-        let isPasswordValid = isValidPassword(passwordText)
-
-        emailLabel.textColor = isEmailValid ? UIColor.black : UIColor(named: "main")
-        passwordLabel.textColor = isPasswordValid ? UIColor.black : UIColor(named: "main")
-
-        updateButtonAppearance(isValidEmail: isEmailValid, isValidPassword: isPasswordValid)
-    }
-
+    
+    
     func isValidEmail(_ email: String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return emailPredicate.evaluate(with: email)
     }
+    
 
     func isValidPassword(_ password: String) -> Bool {
-        return password.count > 8 && !password.isEmpty
+        return password.count > 8
     }
 }
 
--- Letgo/View/RegisterVC.swift
+++ Letgo/View/RegisterVC.swift
@@ -1,35 +1,131 @@
-//
-//  LoginPage.swift
-//  Letgo
-//
-//  Created by abdullah on 22.04.2024.
-//
-
 import UIKit
 
 class RegisterVC: UIViewController {
 
     // MARK: - UI Elements
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var fullNameTextField: UITextField!
+
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+
+    @IBOutlet weak var firstPasswordLabel: UILabel!
+    @IBOutlet weak var firstPasswordTextField: UITextField!
+
+    @IBOutlet weak var confirmPasswordLabel: UILabel!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+
+    @IBOutlet weak var registerButton: UIButton!
     
-    
-    
-    // MARK: - Properties
-    
-    
-    
+    var isFullNameValid = false
+    var isEmailValid = false
+    var isPasswordValid = false
+    var doPasswordsMatch = false
+
     // MARK: - Life Cycle
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        delegateConfiguration()
+        updateButtonAppearance()
     }
     
-    // MARK:  - Functions
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    // MARK: - Function
+    
+    func delegateConfiguration() {
+        fullNameTextField.delegate = self
+        emailTextField.delegate = self
+        firstPasswordTextField.delegate = self
+        confirmPasswordTextField.delegate = self
+        
+    }
     
     
+    func updateButtonAppearance() {
+        if isFullNameValid && isEmailValid && isPasswordValid && doPasswordsMatch {
+            registerButton.isEnabled = true
+            registerButton.backgroundColor = UIColor(named: "main")
+            registerButton.setTitleColor(.white, for: .normal)
+        } else {
+            registerButton.isEnabled = false
+            registerButton.backgroundColor = UIColor.systemGray5
+            registerButton.setTitleColor(UIColor.black, for: .normal)
+        }
+    }
+
     
-    // MARK: - Actions
     
+    func fullNameCheck(_ fullName: String) -> Bool {
+        return fullName.count >= 3
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    func isValidPassword(_ password: String) -> Bool {
+        return password.count > 8
+    }
+
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+
+    }
 
 }
+
+extension RegisterVC: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+
+        if textField == fullNameTextField {
+            let currentText = (textField.text ?? "") as NSString
+            let newText = currentText.replacingCharacters(in: range, with: string)
+            isFullNameValid = fullNameCheck(newText)
+            fullNameTextField.textColor = isFullNameValid ? .black : UIColor(named: "main")
+            fullNameLabel.textColor = isFullNameValid ? .black : UIColor(named: "main")
+        } else if textField == emailTextField {
+            let currentText = (textField.text ?? "") as NSString
+            let newText = currentText.replacingCharacters(in: range, with: string)
+            isEmailValid = isValidEmail(newText)
+            emailTextField.textColor = isEmailValid ? .black : UIColor(named: "main")
+            emailLabel.textColor = isEmailValid ? .black : UIColor(named: "main")
+        } else if textField == firstPasswordTextField || textField == confirmPasswordTextField {
+            let currentText = (textField.text ?? "") as NSString
+            let newText = currentText.replacingCharacters(in: range, with: string)
+            isPasswordValid = isValidPassword(newText)
+            textField.textColor = isPasswordValid ? .black : UIColor(named: "main")
+            
+            if textField == firstPasswordTextField {
+                firstPasswordLabel.textColor = isPasswordValid ? .black : UIColor(named: "main")
+            } else {
+                confirmPasswordLabel.textColor = isPasswordValid ? .black : UIColor(named: "main")
+                doPasswordsMatch = firstPasswordTextField.text == newText
+            }
+        }
+
+        updateButtonAppearance()
+        
+        return true
+    }
+
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            return true
+        }
+   
+
+}
+
